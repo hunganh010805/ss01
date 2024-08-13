0ec7bb21beb751b32af96734c690e9f2bf2e362b
hunganh010805 <147390474+hunganh010805@users.noreply.github.com>
2024-08-13 13:06:05 +0700

    Add files via upload

---
+++ M25/app/baitap/bai5/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from '@/app/components/Button'
+export default function page() {
+  return (
+    <div>
+      <button>Thêm mới</button>
+      <button>Xóa</button>
+      <button>Sửa</button>
+    </div>
+  )
+}
---
+++ M25/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+export default function page() {
+  return (
+    <div>
+      Họ và tên : Nguyễn Thị Thu Quỳnh
+      <Header></Header>
+      <Footer></Footer>
+    </div>
+  )
+}
